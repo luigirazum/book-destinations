b39a27a4f73d8d8d2ab076572052014bcea0ddf3
Obimbo07 <obimboausts@gmail.com>
2024-03-21 06:08:50 +0300

    added new app logo

--- src/components/OffcanvasLayout.jsx
+++ src/components/OffcanvasLayout.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faArrowLeft, faPlus } from '@fortawesome/free-solid-svg-icons';
+import '../stylesheets/off.css';
 
 function OffcanvasLayout({ children }) {
   const [show, setShow] = useState(false);
@@ -19,7 +20,7 @@ function OffcanvasLayout({ children }) {
         <div style={{ position: 'absolute', left: '0', marginLeft: '8px', zIndex: '1001' }}>
           <FontAwesomeIcon
             icon={show ? faArrowLeft : faArrowRight}
-            style={{ color: 'black', cursor: 'pointer', backgroundColor: 'yellow', padding: '8px', borderRadius: show ? '0 8px 8px 0' : '8px 0 0 8px' }}
+            style={{ color: 'black', cursor: 'pointer', padding: '8px', borderRadius: show ? '0 8px 8px 0' : '8px 0 0 8px' }}
             onClick={show ? handleClose : handleShow}
           />
         </div>
@@ -29,23 +30,25 @@ function OffcanvasLayout({ children }) {
         show={show}
         onHide={handleClose}
         placement="start"
-        style={{ backgroundColor: 'yellow', height: '100vh', width: '200px', position: 'fixed', top: 0, zIndex: 1000, marginLeft: show ? '0' : '-200px', transition: 'margin-left 0.3s ease-in-out' }}
+        style={{ height: '100vh', width: '200px', position: 'fixed', top: 0, zIndex: 1000, marginLeft: show ? '0' : '-200px', transition: 'margin-left 0.3s ease-in-out' }}
       >
         <Offcanvas.Header closeButton={!show}>
-          <Navbar.Brand as={Link} to="/" className="align-top">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to="/" className="align-top" style={{ display: 'flex', justifyContent: 'center' }}>
+              <img className='img-fluid' src="/pic.jfif" style={{ width: '98px', height: '89px' }}/> 
+          </Navbar.Brand>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav className="flex-column">
-            <Nav.Link className="nav-links" as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link className="nav-links" as={Link} to="/signup">Signup</Nav.Link>
-            <Nav.Link className="nav-links" as={Link} to="/destinations">Destinations</Nav.Link>
-            <Nav.Link className="nav-links" as={Link} to="/add-destinations" style={{ display: 'flex', flexDirection: 'row', gap: '7px'}}>
- 
+        <Offcanvas.Body style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Nav className="flex-column" style={{ flexDirection: 'column', display: 'flex', gap: '20px' }}>
+            {/* Use NavLink for each link, applying activeClassName */}
+            <NavLink className="nav-link side" activeClassName="active-link" to="/login">Login</NavLink>
+            <NavLink className="nav-link side" activeClassName="active-link" to="/signup">Signup</NavLink>
+            <NavLink className="nav-link side" activeClassName="active-link" to="/destinations">Destinations</NavLink>
+            <NavLink className="nav-link side" activeClassName="active-link" to="/add-destinations" style={{ display: 'flex', flexDirection: 'row', gap: '7px'}}>
               <FontAwesomeIcon icon={faPlus} />
               Add Destinations
-            </Nav.Link>
-            <Nav.Link className="nav-link" as={Link} to="/delete-destinations">Delete Destination</Nav.Link>
-            {/* Add more Nav.Link items here */}
+            </NavLink>
+            <NavLink className="nav-link" activeClassName="active-link" to="/delete-destinations">Delete Destination</NavLink>
+            {/* Add more NavLink items here */}
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
